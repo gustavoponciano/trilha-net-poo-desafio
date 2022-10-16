f9c30582e897a5009b57bb5483efe2b5c663f5e2
gustavoponciano <96.gustavop@gmail.com>
2022-10-16 03:48:22 -0300

    1st try

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Celular Nokia");
+Smartphone nokia = new Nokia();
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+System.Console.WriteLine("Celular iPhone");
+Smartphone iphone = new Iphone();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("iTunes");
